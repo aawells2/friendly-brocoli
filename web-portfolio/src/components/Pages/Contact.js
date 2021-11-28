18e86ebe5ad398cae6a87041ecd025de4d0184be
aawells2 <aawells2@asu.edu>
2021-11-28 13:39:05 -0700

    Update Contact.js
    added JSX. need to add POST and style

--- web-portfolio/src/components/Pages/Contact.js
+++ web-portfolio/src/components/Pages/Contact.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
 
 function Contact(){
-    return <h1>This is the contact page</h1>
+    return(
+        <Container>
+            <h1 className="text-center mt-4">Get in Touch</h1>
+
+            <Form className="mt-5">
+                <Row className="mb-3">
+                    <div className="col-sm-12 col-md-6 col-lg-6">
+                    <Form.Group as={Col} controlId="formGridName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter your name" />
+                    </Form.Group>
+                    </div>
+                    <div className="col-sm-12 col-md-6 col-lg-6">
+                    <Form.Group as={Col} controlId="formGridEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                    </div>
+                </Row>
+
+                <div class="form-group" className="mb-3">
+                    <label for="Textarea">Your Message</label>
+                    <textarea class="form-control" id="Textarea" rows="3" placeholder="Enter your message"></textarea>
+                </div>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </Container>
+
+        );
 }
 
 export default Contact;
